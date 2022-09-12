dc471f09b10604c1df128f74bf0071e96cdd5256
SurendraReddy <91112300+SurendraReddy-6@users.noreply.github.com>
2022-09-12 16:23:03 +0530

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #!/bin/bash
-apt -y install apache2 > /var/log/installapache.out 2>&1
-service apache2 restart > /var/log/restartapache.out 2>&1
+yum install httpd -y > /var/log/installapache.out 2>&1
+systemctl restart httpd > /var/log/restartapache.out 2>&1
