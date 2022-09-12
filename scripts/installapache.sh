a2cb396f4c0f96acb0761f0e0755657940711a20
SurendraReddy <91112300+SurendraReddy-6@users.noreply.github.com>
2022-09-12 16:51:33 +0530

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #!/bin/bash
 yum install httpd -y > /var/log/installhttpd.out 2>&1
-systemctl restart httpd > /var/log/restarthttpd.out 2>&1
+systemctl start httpd > /var/log/starthttpd.out 2>&1
